9c868e1a665fbfe8c3aff6408e45f1d4bce01a9d
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2022-07-25 23:06:51 +0900

    Update pay_calculator.py

--- day1/pay_calculator.py
+++ day1/pay_calculator.py
@@ -1,1 +1,10 @@
+hours = int(input("근무 시간을 입력하세요 : "))
+pay = 9160
 
+if hours > 40 :
+    total_pay = 9160 * hours * 1.5
+
+else :
+    total_pay = 9160 * hours
+
+print("당신의 이번달 월급은 ", total_pay, "입니다.")
